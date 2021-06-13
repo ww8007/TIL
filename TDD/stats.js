932b397a8b765729b08a1225793480ebaa2b9655
ww8007 <wshmin@naver.com>
2021-06-13 18:15:58 +0900

    [study] Learn_several descibe in Test case 여러가지의 테스트가 존재하는 descibe에 대한 학습

--- TDD/stats.js
+++ TDD/stats.js
@@ -2,3 +2,12 @@ exports.max = (numbers) => Math.max(...numbers);
 exports.min = (numbers) => Math.min(...numbers);
 exports.avg = (numbers) =>
   numbers.reduce((acc, cur, index, { length }) => acc + cur / length, 0);
+
+exports.sort = (numbers) => numbers.sort((a, b) => a - b);
+exports.median = (numbers) => {
+  const { length } = numbers;
+  const middle = Math.floor(length / 2);
+  return length % 2
+    ? numbers[middle]
+    : (numbers[middle - 1] + numbers[middle]) / 2;
+};
--- TDD/stats.test.js
+++ TDD/stats.test.js
@@ -10,4 +10,15 @@ describe("stats", () => {
   it("gets avg value", () => {
     expect(stats.avg([1, 2, 3, 4, 5])).toBe(3);
   });
+  describe("median", () => {
+    it("sorts the array", () => {
+      expect(stats.sort([5, 4, 1, 2, 3])).toEqual([1, 2, 3, 4, 5]);
+    });
+    it("gets the median for odd length", () => {
+      expect(stats.median([1, 2, 3, 4, 5])).toBe(3);
+    });
+    it("gets the median for even length", () => {
+      expect(stats.median([1, 2, 3, 4, 5, 6])).toBe(3.5);
+    });
+  });
 });
