568a0d4d4186394c202dd3133083746dce9ef570
ww8007 <wshmin@naver.com>
2021-06-14 01:06:51 +0900

    [refactor] Add_useCallback_Optimize

--- rtl-tdd-todos/src/TodoItem.js
+++ rtl-tdd-todos/src/TodoItem.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 const TodoItem = ({ todo, onToggle, onRemove }) => {
    const { id, text, done } = todo;
+   const toggle = useCallback(() => onToggle(id), [id, onToggle]);
+   const remove = useCallback(() => onRemove(id), [id, onRemove]);
+
    return (
       <li>
          <span
             style={{ textDecoration: done ? "line-through" : "none" }}
-            onClick={() => onToggle(id)}
+            onClick={toggle}
          >
             {text}
          </span>
-         <button onClick={() => onRemove(id)}>삭제</button>
+         <button onClick={remove}>삭제</button>
       </li>
    );
 };
