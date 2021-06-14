a2ca2d35e31671952a6cd397691217f9d9cead5a
ww8007 <wshmin@naver.com>
2021-06-15 03:05:42 +0900

    [feat] Add_store_with_Provider

--- React/Redux/리덕스 기초 이해/src/index.js
+++ React/Redux/리덕스 기초 이해/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import rootReducer from './modules/index';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
