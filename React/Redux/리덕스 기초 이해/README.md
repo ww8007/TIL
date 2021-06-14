59e9b1647fd1b43b281562f380736d1e195c0288
ww8007 <wshmin@naver.com>
2021-06-15 03:30:55 +0900

    [refactor] use_redux_actions

--- React/Redux/리덕스 기초 이해/README.md
+++ React/Redux/리덕스 기초 이해/README.md
@@ -259,3 +259,34 @@ export default connect(
 
 - 액션 생성 함수를 더 짧은 코드로 작성이 가능
 - 리듀서를 작성할 때도 switch/case 문이 아닌 handleActions라는 함수를 사용하여 각 액션 마다 업데이트 함수를 설정하는 형식으로 작성 가능
+
+## counter 모듈에 적용
+
+- 매번 객체를 만들어줄 필요가 없이 간단하게 액션 생성 함수를 선언이 가능
+
+- createAction
+
+```js
+import { createAction } from 'redux-actions';
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+```
+
+- handleActions
+  - 첫 번째 파라미터에 각 액션에 대한 업데이트 함수
+  - 두 번째 파라미터에 초기 상태를 넣어줌
+
+> 코드가 정리되어 initialState가 파라미터 처럼 안보이지만 파라미터가 맞으므로 빼먹지 않도록 한다.
+
+```js
+import { handleActions } from 'redux-actions';
+const counter = handleActions({
+  [INCREASE]: (state, action) => ({
+    number: state.number + 1,
+  }),
+  [DECREASE]: (state, action) => ({
+    number: state.number - 1,
+  }),
+  initialState,
+});
+```
--- React/Redux/리덕스 기초 이해/src/modules/counter.js
+++ React/Redux/리덕스 기초 이해/src/modules/counter.js
@@ -1,28 +1,25 @@
 /** @format */
+import { createAction } from 'redux-actions';
+import { handleActions } from 'redux-actions';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 const initialState = {
   number: 0,
 };
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions({
+  [INCREASE]: (state, action) => ({
+    number: state.number + 1,
+  }),
+  [DECREASE]: (state, action) => ({
+    number: state.number - 1,
+  }),
+  initialState,
+});
 
 export default counter;
