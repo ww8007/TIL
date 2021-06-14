14f449ff7522abe95d92700bd6ea569c57623bb5
ww8007 <wshmin@naver.com>
2021-06-15 03:04:41 +0900

    [feat] Add_Todos 등록, 삭제, 줄긋기 기능이 있는 컴포넌트 추가

---
+++ React/Redux/리덕스 기초 이해/src/components/Todos.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const TodoItem = ({ todo, onToggle, onRemove }) => {
+  return (
+    <div>
+      <input
+        type="checkbox"
+        onClick={() => onToggle(todo.id)}
+        readOnly={true}
+      />
+      <span style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>
+        예제 텍스트
+      </span>
+      <button onClick={() => onRemove(todo.id)}>삭제</button>
+    </div>
+  );
+};
+
+const Todos = ({
+  input,
+  todos,
+  onChangeInput,
+  onInsert,
+  onToggle,
+  onRemove,
+}) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onInsert(input);
+    onChangeInput('');
+  };
+  const onChange = (e) => {
+    onChangeInput(e.target.value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={input} onChange={onChange} />
+        <button type="submit">등록</button>
+      </form>
+      {todos.map((todo) => (
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          onToggle={onToggle}
+          onRemove={onRemove}
+        ></TodoItem>
+      ))}
+    </div>
+  );
+};
+
+export default Todos;
