b4a6c66c08de92457f54bd21f4bd20afbe63bf77
ww8007 <wshmin@naver.com>
2021-06-15 02:35:08 +0900

    [feat] Add_CounterContainer connect를 사용하여 mapStateToProps, mapDispatchToProps를 이용하여 컴포넌트를 리덕스에 연결

---
+++ React/Redux/리덕스 기초 이해/src/containers/CounterContainer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { decrease, increase } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+const mapStateToProps = (state) => ({
+  number: state.counter.number,
+});
+const mapDispatchToProps = (dispatch) => ({
+  increase: () => {
+    dispatch(increase());
+  },
+  decrease: () => {
+    dispatch(decrease());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
