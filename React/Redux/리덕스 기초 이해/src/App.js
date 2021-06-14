3b035b0cc9a0863943af3085f60b6673fc4e4d1f
ww8007 <wshmin@naver.com>
2021-06-15 03:05:02 +0900

    [feat] Add_CounterContainer, Todos

--- React/Redux/리덕스 기초 이해/src/App.js
+++ React/Redux/리덕스 기초 이해/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Counter from './components/Counter';
+import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CounterContainer />
+      <hr />
+      <Todos />
     </div>
   );
-}
+};
 
 export default App;
