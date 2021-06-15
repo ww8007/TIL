a6eac77da524d060058ea7c9eb168eac7af7cc34
ww8007 <wshmin@naver.com>
2021-06-15 17:35:33 +0900

    [feat] Add_TodosContainer

--- React/Redux/리덕스 기초 이해/src/App.js
+++ React/Redux/리덕스 기초 이해/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import Counter from './components/Counter';
-import Todos from './components/Todos';
 import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
 
 const App = () => {
   return (
     <div>
       <CounterContainer />
       <hr />
-      <Todos />
+      <TodosContainer />
     </div>
   );
 };
