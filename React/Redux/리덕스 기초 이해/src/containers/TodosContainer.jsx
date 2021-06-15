d013568a27d58ba5752dc724e89752d53d34079b
ww8007 <wshmin@naver.com>
2021-06-15 17:34:56 +0900

    [refactor] useActions 여러개의 액션들을 모아서 사용

---
+++ React/Redux/리덕스 기초 이해/src/containers/TodosContainer.jsx
@@ -0,0 +1,28 @@
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeInput, insert, remove, toggle } from '../modules/todos';
+import Todos from '../components/Todos';
+import useActions from '../lib/useActions';
+
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
+  return (
+    <Todos
+      input={input}
+      todos={todos}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
+    />
+  );
+};
+
+export default TodosContainer;
