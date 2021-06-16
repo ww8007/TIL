a1d44e816bef2010d10910150535159d2cc8acd2
ww8007 <wshmin@naver.com>
2021-06-16 18:26:37 +0900

    [feat] Add_loading_module 로딩에 대한 모듈 추가

---
+++ React/MiddleWare/미들웨어 직접 만들어보기/src/modules/loading.js
@@ -0,0 +1,30 @@
+import { createAction, handleActions } from 'redux-actions';
+const START_LOADING = 'loading/START_LOADING';
+const FINISH_LOADING = 'loading/FINISH_LOADING';
+
+export const startLoading = createAction(
+  START_LOADING,
+  (requestType) => requestType
+);
+export const endLoading = createAction(
+  FINISH_LOADING,
+  (requestType) => requestType
+);
+
+const initialState = {};
+
+const loading = handleActions(
+  {
+    [START_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: true,
+    }),
+    [FINISH_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: false,
+    }),
+  },
+  initialState
+);
+
+export default loading;
