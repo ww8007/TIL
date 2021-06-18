11a3e5d950605650d2d9c0b5c139c1cd1a204143
ww8007 <wshmin@naver.com>
2021-06-18 20:06:15 +0900

    [refactor] createAction -> createRequestSaga 훅을 사용해서 리펙토링

--- React/MiddleWare/redux-saga/redux-saga-ex/src/module/sample.js
+++ React/MiddleWare/redux-saga/redux-saga-ex/src/module/sample.js
@@ -2,14 +2,13 @@ import { handleActions, createAction } from 'redux-actions';
 import * as api from '../lib/api';
 import { endLoading, startLoading } from './loading';
 import { put, call, takeLatest } from 'redux-saga/effects';
+import createRequestSaga from '../hooks/createRequestSaga';
 // 액션 타입 선언
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 // thunk 함수 생성,
 // thunk 함수 내부에서는 시작할 때 성공했을 때 실패 시 각각 다른 액션을 디스패치
@@ -17,41 +16,8 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 export const getPost = createAction(GET_POST, (id) => id);
 export const getUsers = createAction(GET_USERS);
 
-function* getPostSaga(action) {
-  yield put(startLoading(GET_POST));
-  try {
-    const post = yield call(api.getPost, action.payload);
-    yield put({
-      type: GET_POST_SUCCESS,
-      payload: post.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-  yield put(endLoading(GET_POST));
-}
-
-function* getUserSaga() {
-  yield put(startLoading(GET_USERS));
-  try {
-    const users = yield call(api.getUsers);
-    yield put({
-      type: GET_USERS_SUCCESS,
-      payload: users.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-  yield put(endLoading(GET_USERS));
-}
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUserSaga = createRequestSaga(GET_USERS, api.getUsers);
 
 export function* sampleSaga() {
   yield takeLatest(GET_POST, getPostSaga);
