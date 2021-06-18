c196193392c5473c5cbba2de6f87d29ef045c836
ww8007 <wshmin@naver.com>
2021-06-18 20:05:01 +0900

    [feat] Add_createRequestSaga_Hook

---
+++ React/MiddleWare/redux-saga/redux-saga-ex/src/hooks/createRequestSaga.js
@@ -0,0 +1,24 @@
+import { call, put } from 'redux-saga/effects';
+import { startLoading, endLoading } from '../module/loading';
+
+export default function createRequestSaga(type, request) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+  return function* (action) {
+    yield put(startLoading(type));
+    try {
+      const response = yield call(request, action.payload);
+      yield put({
+        type: SUCCESS,
+        payload: response.data,
+      });
+    } catch (e) {
+      yield put({
+        type: FAILURE,
+        payload: e,
+        error: true,
+      });
+    }
+    yield put(endLoading(type));
+  };
+}
