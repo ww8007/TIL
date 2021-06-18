e3e62794cdbab2db2c8a48a9a4173e3f73b7ec9e
ww8007 <wshmin@naver.com>
2021-06-19 02:55:35 +0900

    [refactor] Add_Sample_Hook

--- React/MiddleWare/redux-saga/redux-saga-ex/src/components/SampleContainer.jsx
+++ React/MiddleWare/redux-saga/redux-saga-ex/src/components/SampleContainer.jsx
@@ -1,46 +1,7 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useSample } from '../hooks/useSample';
-import { getPost, getUsers } from '../module/sample';
+import React from 'react';
+import Sample from '../hooks/useSample';
 const SampleContainer = () => {
-  useEffect(() => {});
-  const { post, users, loadingPost, loadingUsers } = useSample();
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getPost(1));
-    // dispatch(getUsers());
-  });
-  return (
-    <>
-      <section>
-        {!loadingPost && (
-          <div>
-            <>
-              <h3>{post.title}</h3>
-              <h3>{post.body}</h3>
-            </>
-          </div>
-        )}
-        {loadingPost && '로딩중...'}
-      </section>
-      <hr />
-      {/* <section>
-        {loadingUsers && (
-          <>
-            <h1>사용자 목록</h1>
-            <ul>
-              {users.map((user) => (
-                <li key={user.id}>
-                  {user.id}({user.email})
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-      </section> */}
-      {/* {!loadingUsers && '로딩중...'} */}
-    </>
-  );
+  return <Sample />;
 };
 
-export default React.memo(SampleContainer);
+export default SampleContainer;
