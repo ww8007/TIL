ecb6f5d5dec1ec067d331ac89aa83ef3d04afac8
ww8007 <wshmin@naver.com>
2021-06-19 02:55:14 +0900

    [fix] Delete_id

--- React/MiddleWare/redux-saga/redux-saga-ex/src/lib/api.js
+++ React/MiddleWare/redux-saga/redux-saga-ex/src/lib/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const getPost = (id) =>
   axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
-export const getUsers = (id) =>
+export const getUsers = () =>
   axios.get(`https://jsonplaceholder.typicode.com/users`);
