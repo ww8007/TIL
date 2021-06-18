243905e87e3b7fa9622fea6a2db2c2057fba07f4
ww8007 <wshmin@naver.com>
2021-06-18 19:45:20 +0900

    [feat] Add_SampleContainer

--- React/MiddleWare/redux-saga/redux-saga-ex/src/App.js
+++ React/MiddleWare/redux-saga/redux-saga-ex/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import CounterContainer from './components/CounterContainer';
-
+import SampleContainer from './components/SampleContainer';
 const App = () => {
-  return <CounterContainer />;
+  return <SampleContainer />;
 };
 
 export default App;
