7c6ba7d5d5f27c4f4c5deca3a85a5bebd3d48789
ww8007 <wshmin@naver.com>
2021-06-18 20:05:33 +0900

    [feat] Add_loading_rootReducer

--- React/MiddleWare/redux-saga/redux-saga-ex/src/module/index.js
+++ React/MiddleWare/redux-saga/redux-saga-ex/src/module/index.js
@@ -2,9 +2,12 @@ import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import counter, { counterSaga } from './counter';
 import sample, { sampleSaga } from './sample';
+import loading from './loading';
+
 const rootReducer = combineReducers({
   counter,
   sample,
+  loading,
 });
 export function* rootSaga() {
   yield all([counterSaga(), sampleSaga()]);
