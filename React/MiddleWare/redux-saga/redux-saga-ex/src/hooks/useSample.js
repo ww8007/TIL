8875c0b7ef4477ab0bf701f3a59e3a7a8b27f884
ww8007 <wshmin@naver.com>
2021-06-19 02:56:26 +0900

    [fix] Add_post&&, users&& post 존재와 users 존재 확인 버그 수정

--- React/MiddleWare/redux-saga/redux-saga-ex/src/hooks/useSample.js
+++ React/MiddleWare/redux-saga/redux-saga-ex/src/hooks/useSample.js
@@ -10,11 +10,51 @@ export function useSample() {
       loadingUsers: loading['sample/GET_USERS'],
     })
   );
-
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPost(1));
+    dispatch(getUsers());
+  }, [dispatch]);
   return {
     post,
     users,
     loadingPost,
     loadingUsers,
   };
+}
+
+export default function Sample() {
+  const { post, users, loadingPost, loadingUsers } = useSample();
+  return (
+    <>
+      <section>
+        {console.log(post)}
+        {loadingPost && '로딩중...'}
+        {!loadingPost && post && (
+          <div>
+            <>
+              <h3>{post.title}</h3>
+              <h3>{post.body}</h3>
+            </>
+          </div>
+        )}
+      </section>
+      <hr />
+      <section>
+        {loadingUsers && '로딩중...'}
+        {!loadingUsers && users && (
+          <>
+            <h1>사용자 목록</h1>
+            <ul>
+              {users.map((user) => (
+                <li key={user.id}>
+                  {user.id}({user.email})
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </section>
+    </>
+  );
 }
