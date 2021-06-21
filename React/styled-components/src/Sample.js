a7dea94cc1a2ac1ff5b8ae7a0f0c25dfe62c4468
ww8007 <wshmin@naver.com>
2021-06-21 21:21:26 +0900

    [study/front] add-Sample

---
+++ React/styled-components/src/Sample.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const Box = styled.div`
+  background: ${(props) => props.color || 'blue'};
+  padding: 1rem;
+  display: flex;
+
+  width: 1024px;
+  margin: 0 auto;
+  @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768) {
+    width: 100%;
+  }
+
+  ${(props) =>
+    props.inverted &&
+    css`
+      background: none;
+      border: 2px solid white;
+      color: white;
+      &:hover {
+        background: white;
+        color: black;
+      }
+    `}
+`;
+
+const Sample = ({ className }) => {
+  return <div className={className}>Sample</div>;
+};
+
+export default Sample;
