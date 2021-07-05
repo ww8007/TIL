305645eb9b9fcddf603a45a6854d934b95469b4d
ww8007 <wshmin@naver.com>
2021-07-05 23:31:32 +0900

    [feat] add-graphql-server

---
+++ GraphQL/Example/index.js
@@ -0,0 +1,53 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { graphExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const PORT = 8000;
+
+const app = express();
+
+const typeDefs = `
+   type Lang {
+       id:Int,
+       name:String!,  
+   }
+   type Query {
+       getLangs(name:String):[Lang]
+   }
+`;
+
+const langs = [
+  {
+    id: 0,
+    name: 'Node',
+  },
+  {
+    id: 1,
+    name: 'Java',
+  },
+];
+
+const resolvers = {
+  Query: {
+    getLangs: () => langs, //getLangs요청을 받으면 langs배열을 응답해줄 것입니다.
+  },
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+app.use('/graphql', bodyParser.json(), graphExpress({ schema }));
+
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpoint: '/graphiql',
+  })
+);
+
+app.listen(PORT, () => console.log(`SERVER RUNNING AT ${PORT}`));
