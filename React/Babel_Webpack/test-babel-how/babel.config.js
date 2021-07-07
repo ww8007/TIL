ced5c1f87cfa5fd881b0d45988ad1947c9e15407
ww8007 <wshmin@naver.com>
2021-07-07 13:50:38 +0900

    [add] Babel.config.js

---
+++ React/Babel_Webpack/test-babel-how/babel.config.js
@@ -0,0 +1,6 @@
+const presets = ['@babel/preset-react'];
+const plugins = [
+  '@babel/plugin-transform-template-literals',
+  '@babel/plugin-transform-arrow-functions',
+];
+module.exports = { presets, plugins };
