a3767857b70a58b94bc65929543f215d2473dadc
ww8007 <wshmin@naver.com>
2021-07-07 13:51:34 +0900

    [study] babel-dist-example

---
+++ React/Babel_Webpack/test-babel-how/dist/code.js
@@ -0,0 +1,6 @@
+const element = /*#__PURE__*/React.createElement("div", null, "babel test");
+const text = "element type is ".concat(element.type);
+
+const add = function (a, b) {
+  return a + b;
+};
