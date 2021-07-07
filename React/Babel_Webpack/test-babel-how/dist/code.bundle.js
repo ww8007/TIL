3b1b95ae3069365a3714970a2245c580b076340c
ww8007 <wshmin@naver.com>
2021-07-07 14:33:24 +0900

    [study] dist-webpack

---
+++ React/Babel_Webpack/test-babel-how/dist/code.bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+const element = /*#__PURE__*/React.createElement("div", null, "babel test");
+const text = "element type is ".concat(element.type);
+
+const add = function (a, b) {
+  return a + b;
+};
+/******/ })()
+;
