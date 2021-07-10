3a31d6e212ef7874f3dc0ad145ef165bb64b5cf3
ww8007 <wshmin@naver.com>
2021-07-10 13:54:52 +0900

    [study] url-loader 이미지 크기 조정 limit 옵션보다 크면 주소 경로 입력 limit 옵션보다 작으면 파일 경로

--- React/Babel_Webpack/Webpack/Begginer/webpack-loader/webpack.config.js
+++ React/Babel_Webpack/Webpack/Begginer/webpack-loader/webpack.config.js
@@ -19,7 +19,14 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|gif)$/,
-        use: 'file-loader',
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 100000000,
+            },
+          },
+        ],
       },
       {
         test: /\.txt$/,
