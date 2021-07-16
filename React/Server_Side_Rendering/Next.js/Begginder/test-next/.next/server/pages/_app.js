0b590e4f9bb3d734a627df60d9cc9ed9f3efdc54
ww8007 <wshmin@naver.com>
2021-07-16 13:59:21 +0900

    study : 넥스트js 기본 알아보기

---
+++ React/Server_Side_Rendering/Next.js/Begginder/test-next/.next/server/pages/_app.js
@@ -0,0 +1,95 @@
+(function() {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 318:
+/***/ (function(module) {
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : {
+    "default": obj
+  };
+}
+
+module.exports = _interopRequireDefault;
+
+/***/ }),
+
+/***/ 857:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _interopRequireDefault = __webpack_require__(318);
+
+exports.__esModule = true;
+exports.default = void 0;
+
+var _react = _interopRequireDefault(__webpack_require__(297));
+
+var _utils = __webpack_require__(579);
+
+exports.AppInitialProps = _utils.AppInitialProps;
+exports.NextWebVitalsMetric = _utils.NextWebVitalsMetric;
+/**
+* `App` component is used for initialize of pages. It allows for overwriting and full control of the `page` initialization.
+* This allows for keeping state between navigation, custom error handling, injecting additional data.
+*/
+
+async function appGetInitialProps({
+  Component,
+  ctx
+}) {
+  const pageProps = await (0, _utils.loadGetInitialProps)(Component, ctx);
+  return {
+    pageProps
+  };
+}
+
+class App extends _react.default.Component {
+  render() {
+    const {
+      Component,
+      pageProps
+    } = this.props;
+    return /*#__PURE__*/_react.default.createElement(Component, pageProps);
+  }
+
+}
+
+exports.default = App;
+App.origGetInitialProps = appGetInitialProps;
+App.getInitialProps = appGetInitialProps;
+
+/***/ }),
+
+/***/ 579:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dist/next-server/lib/utils.js");;
+
+/***/ }),
+
+/***/ 297:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("react");;
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+var __webpack_exports__ = (__webpack_exec__(857));
+module.exports = __webpack_exports__;
+
+})();
---
+++ React/Server_Side_Rendering/Next.js/Begginder/test-next/pages/page1.js
@@ -0,0 +1,9 @@
+function Page1() {
+  return (
+    <div>
+      <p>This is Home Page</p>
+    </div>
+  );
+}
+
+export default Page1;
