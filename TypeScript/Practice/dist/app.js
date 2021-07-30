c3ad7d8b7f5f37ce2959cbb537267e94c84be828
ww8007 <wshmin@naver.com>
2021-07-30 18:53:31 +0900

    Study : enum type

--- TypeScript/Practice/dist/app.js
+++ TypeScript/Practice/dist/app.js
@@ -1,7 +1,7 @@
 var Fruit;
 (function (Fruit) {
-    Fruit[(Fruit['Apple'] = 0)] = 'Apple';
-    Fruit[(Fruit['Banana'] = 5)] = 'Banana';
-    Fruit[(Fruit['Orange'] = 5)] = 'Orange';
+  Fruit[(Fruit['Apple'] = 0)] = 'Apple';
+  Fruit[(Fruit['Banana'] = 5)] = 'Banana';
+  Fruit[(Fruit['Orange'] = 5)] = 'Orange';
 })(Fruit || (Fruit = {}));
 console.log(Fruit.Apple, Fruit.Banana, Fruit.Orange);
