6f431a0b1a8e28f9d36fc2179cebc08544ce8034
ww8007 <wshmin@naver.com>
2021-07-31 15:45:38 +0900

    Study : non-overload -> error 오버로드를 사용하지 않으면 오류 생김

---
+++ TypeScript/Practice/function/non-overload.ts
@@ -0,0 +1,10 @@
+function add(x: number | string, y: number | string): number | string {
+  if (typeof x === 'number' && typeof y === 'number') {
+    return x + y;
+  } else {
+    const result = Number(x) | Number(y);
+    return result.toString();
+  }
+}
+// const v1: number = add(1, 2);
+console.log(add(1, '2'));
