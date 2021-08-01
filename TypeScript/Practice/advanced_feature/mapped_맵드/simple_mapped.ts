aa26fe4b2e69dc6d5f134181130604028f78293c
ww8007 <wshmin@naver.com>
2021-08-01 22:43:01 +0900

    Study : example of mapped

---
+++ TypeScript/Practice/advanced_feature/mapped_맵드/simple_mapped.ts
@@ -0,0 +1,12 @@
+interface MPerson {
+  name: string;
+  age: number;
+}
+interface PersonOptional {
+  name?: string;
+  age?: number;
+}
+interface PersonReadOnly {
+  readonly name: string;
+  readonly age: number;
+}
