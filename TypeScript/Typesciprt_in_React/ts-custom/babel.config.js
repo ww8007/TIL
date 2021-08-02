6e2da3d5f5a0396bb406d53a646d8a525df28520
ww8007 <wshmin@naver.com>
2021-08-02 21:41:19 +0900

    Feat : add babel.config

---
+++ TypeScript/Typesciprt_in_React/ts-custom/babel.config.js
@@ -0,0 +1,15 @@
+const presets = [
+  [
+    '@babel/preset-env',
+
+    {
+      targets: {
+        chrome: '40',
+      },
+      useBuiltIns: 'usage',
+      corejs: { version: 3, proposals: true },
+    },
+  ],
+];
+
+module.exports = { presets };
