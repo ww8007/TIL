2c602a1d53f6d147b210af05ff7b26bba95d4fb3
ww8007 <wshmin@naver.com>
2021-08-02 21:44:20 +0900

    Study : 직접 타입스크립트 설정

--- TypeScript/Typesciprt_in_React/ts-custom/src/App.tsx
+++ TypeScript/Typesciprt_in_React/ts-custom/src/App.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from 'react';
-
+import { getValue } from './legacy';
+import Icon from './icon.jpg';
+// window.myValue = 123;
 export default function App({
   name,
   age,
@@ -7,8 +9,11 @@ export default function App({
   name: string;
   age: number;
 }): ReactElement {
+  const value = getValue();
+  console.log(value.toFixed(2));
   return (
     <div>
+      <img src={Icon} />
       <p>{name}</p>
       <p>{age}</p>
     </div>
--- TypeScript/Typesciprt_in_React/ts-custom/src/index.tsx
+++ TypeScript/Typesciprt_in_React/ts-custom/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import _ from 'lodash';
 
-ReactDOM.render(
-  <App age={23} name={'jang'} />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App age={3} name="23" />, document.getElementById('root'));
+
+// console.log('123'.padStart(5, '0'));
