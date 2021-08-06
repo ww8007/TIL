bbb9c963e8237887b822f5e3c7888f1caa94e3d8
ww8007 <wshmin@naver.com>
2021-08-06 14:25:00 +0900

    Study : add simple render in R/N

--- React_Native/Do_it_Book/ch02_basic/ch02/App.tsx
+++ React_Native/Do_it_Book/ch02_basic/ch02/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
-
+import * as D from './src/data';
+const person = D.createRandomPerson();
 export default function App() {
   return (
     <SafeAreaView>
-      <Text>JSX world!</Text>
+      <Text>{JSON.stringify(person, null, 2)}</Text>
     </SafeAreaView>
   );
 }
---
+++ React_Native/Do_it_Book/ch02_basic/ch02/src/data/IPerson.ts
@@ -0,0 +1,18 @@
+type IPerson = {
+  id: string;
+  createdDate: Date;
+  modifiedDate: Date;
+  name: string;
+  email: string;
+  avatar: string;
+  image: string;
+  comments: string;
+  counts: {
+    comment: number;
+    retweet: number;
+    heart: number;
+  };
+};
+
+// eslint-disable-next-line no-undef
+export type {IPerson};
