469d5c77c052546bc46d7e1dd9ab48175aee0cb1
ww8007 <wshmin@naver.com>
2021-08-07 15:50:40 +0900

    Study : add TextInput onChangeText : call back함수 시그니처, 함수 타입 : 함수 선언문에서 이름만 제외한 부분을 이렇게 지칭

--- React_Native/Do_it_Book/ch02_basic/ch02_event/App.tsx
+++ React_Native/Do_it_Book/ch02_basic/ch02_event/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import {SafeAreaView, Alert} from 'react-native';
-import {Text} from 'react-native';
+import {SafeAreaView, Alert, Button, TextInput} from 'react-native';
+import {TouchableOpacity, TouchableHighlight, Text} from 'react-native';
 
 const onPress = () => Alert.alert('home pressed', 'message');
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Text onPress={onPress}>Press me</Text>
+      <Button title="Home" onPress={onPress} />
+      <TouchableOpacity onPress={onPress}>
+        <Text>TouchableOpacity</Text>
+      </TouchableOpacity>
+      <TouchableHighlight onPress={onPress}>
+        <Text>TouchableHighlight</Text>
+      </TouchableHighlight>
+      <TextInput
+        placeholder="enter your name"
+        onChangeText={(text: string) => console.log(text)}
+        onFocus={() => console.log('onFocus')}
+        onBlur={() => console.log('onBlur')}
+        onEndEditing={() => console.log('end edit')}
+      />
     </SafeAreaView>
   );
 }
