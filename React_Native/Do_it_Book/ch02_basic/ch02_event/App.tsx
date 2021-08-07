ca9dffc50ed9497bf31175123679a358ba5ec07f
ww8007 <wshmin@naver.com>
2021-08-07 15:30:41 +0900

    Study : Text component can use onPress event

--- React_Native/Do_it_Book/ch02_basic/ch02_event/App.tsx
+++ React_Native/Do_it_Book/ch02_basic/ch02_event/App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import {SafeAreaView, Alert, Button} from 'react-native';
-import {TouchableOpacity, TouchableHighlight, Text} from 'react-native';
+import {SafeAreaView, Alert} from 'react-native';
+import {Text} from 'react-native';
 
 const onPress = () => Alert.alert('home pressed', 'message');
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Button title="Home" onPress={onPress} />
-      <TouchableOpacity onPress={onPress}>
-        <Text>TouchableOpacity</Text>
-      </TouchableOpacity>
-      <TouchableHighlight onPress={onPress}>
-        <Text>TouchableHighlight</Text>
-      </TouchableHighlight>
+      <Text onPress={onPress}>Press me</Text>
     </SafeAreaView>
   );
 }
