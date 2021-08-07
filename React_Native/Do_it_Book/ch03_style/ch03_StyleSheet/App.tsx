24f1c22327788af6b798967ad007033e8aa00f17
ww8007 <wshmin@naver.com>
2021-08-07 17:01:51 +0900

    Feat : react-native init

---
+++ React_Native/Do_it_Book/ch03_style/ch03_StyleSheet/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: 'blue'}}>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+}
