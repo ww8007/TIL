723ed63d02e24afbff9f7b76452f4f0d0efbe39e
ww8007 <wshmin@naver.com>
2021-08-07 15:29:21 +0900

    Study : add TouchableOpacity, TouchableHighlight TouchableOpacity : 글자 투명도가 터치 시 바뀜 TouchableHighlight : background 색 변경

--- React_Native/Do_it_Book/ch02_basic/ch02_event/App.tsx
+++ React_Native/Do_it_Book/ch02_basic/ch02_event/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {SafeAreaView, Alert, Button} from 'react-native';
+import {TouchableOpacity, TouchableHighlight, Text} from 'react-native';
+
+const onPress = () => Alert.alert('home pressed', 'message');
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Button
-        title="Home"
-        onPress={() => Alert.alert('home pressed', 'message')}
-      />
+      <Button title="Home" onPress={onPress} />
+      <TouchableOpacity onPress={onPress}>
+        <Text>TouchableOpacity</Text>
+      </TouchableOpacity>
+      <TouchableHighlight onPress={onPress}>
+        <Text>TouchableHighlight</Text>
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
