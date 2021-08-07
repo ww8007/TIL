8fecaba23c0c6714249ec7c839cd4d07facce35e
ww8007 <wshmin@naver.com>
2021-08-07 17:18:15 +0900

    Study : add color use in StyleSheet

--- React_Native/Do_it_Book/ch03_style/ch03_StyleSheet/App.tsx
+++ React_Native/Do_it_Book/ch03_style/ch03_StyleSheet/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import {Text, SafeAreaView} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {Colors} from 'react-native-paper';
+import Color from 'color';
+
+console.log(Colors.blue500);
+console.log(Color(Colors.blue500).alpha(0.5).lighten(0.5).string());
 
 export default function App() {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: 'blue'}}>
-      <Text>Hello</Text>
+    <SafeAreaView style={[styles.safeAreaView]}>
+      <Text style={[styles.text]}>Hello</Text>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.blue500,
+  },
+  text: {
+    fontSize: 20,
+    color: Color(Colors.blue500).alpha(0.7).lighten(0.9).string(),
+  },
+});
