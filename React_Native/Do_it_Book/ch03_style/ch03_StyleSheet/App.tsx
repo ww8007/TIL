147ce89af0dd1cf979dfdc9f5de3b9dc60715843
ww8007 <wshmin@naver.com>
2021-08-08 14:09:15 +0900

    Study : add box, border, Platform.select Platform 마다 ios → padding 동작안함 margin으로 해결 android → padding으로 그대로 해결

--- React_Native/Do_it_Book/ch03_style/ch03_StyleSheet/App.tsx
+++ React_Native/Do_it_Book/ch03_style/ch03_StyleSheet/App.tsx
@@ -1,29 +1,46 @@
 import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
-import {StyleSheet} from 'react-native';
+import {Text, SafeAreaView, View} from 'react-native';
+import {StyleSheet, Dimensions, Platform} from 'react-native';
 import {Colors} from 'react-native-paper';
-import Color from 'color';
+import color from 'color';
 
-console.log(Colors.blue500);
-console.log(Color(Colors.blue500).alpha(0.5).lighten(0.5).string());
-
+const {width, height} = Dimensions.get('window');
 export default function App() {
   return (
     <SafeAreaView style={[styles.safeAreaView]}>
-      <Text style={[styles.text]}>Hello</Text>
+      <Text style={[styles.text]}>os: {Platform.OS}</Text>
+      <Text style={[styles.text]}>width : {width}</Text>
+      <Text style={[styles.text]}>height : {height}</Text>
+
+      <View style={[styles.box, {borderRadius: 10}]} />
+      <View style={[styles.box, styles.border]} />
+      <View style={[styles.box, styles.border, {borderRadius: 10}]} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   safeAreaView: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: Colors.blue500,
+    flex: 1,
+    paddingLeft: Platform.select({ios: 0, android: 20}),
   },
   text: {
+    marginBottom: 10,
+    marginLeft: 50,
     fontSize: 20,
-    color: Color(Colors.blue500).alpha(0.7).lighten(0.9).string(),
+    marginTop: 20,
+    color: color(Colors.blue500).alpha(0.7).lighten(0.9).string(),
+  },
+  box: {
+    width: '70%',
+    height: 100,
+    backgroundColor: Colors.lime500,
+    marginBottom: 10,
+    marginLeft: Platform.select({ios: 20, android: 0}),
+  },
+  border: {
+    borderWidth: 10,
+    borderColor: color('black').alpha(0.3).string(),
   },
 });
