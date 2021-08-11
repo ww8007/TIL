fe492191fc600d6b75ba6eb74ccb0dc267dced03
ww8007 <wshmin@naver.com>
2021-08-11 17:00:14 +0900

    Study : add Optimize with React Hook

---
+++ React_Native/Do_it_Book/ch04_hook/ch04_useMemo/src/components/TouchableView.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import type {FC, ComponentProps} from 'react';
+import {TouchableOpacity, View} from 'react-native';
+import type {StyleProp, ViewStyle} from 'react-native';
+type TouchableOpacityProps = ComponentProps<typeof TouchableOpacity>;
+
+export type TouchableViewProps = TouchableOpacityProps & {
+  viewStyle?: StyleProp<ViewStyle>;
+};
+
+export const TouchableView: FC<TouchableViewProps> = ({
+  children,
+  viewStyle,
+  ...touchableProps
+}) => {
+  return (
+    <TouchableOpacity {...touchableProps}>
+      <View style={[viewStyle]}>{children}</View>
+    </TouchableOpacity>
+  );
+};
---
+++ React_Native/Do_it_Book/ch04_hook/ch04_useMemo/src/data/index.ts
@@ -0,0 +1,4 @@
+export * from './util';
+export * from './faker';
+export * from './IPerson';
+export * from './createRandomPerson';
