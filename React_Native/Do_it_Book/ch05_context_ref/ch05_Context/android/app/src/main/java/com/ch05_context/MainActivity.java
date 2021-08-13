f89d22b764965ea3595c9052f96435c7f504d783
ww8007 <wshmin@naver.com>
2021-08-13 18:30:47 +0900

    Feat : react-naitve init Context

---
+++ React_Native/Do_it_Book/ch05_context_ref/ch05_Context/android/app/src/main/java/com/ch05_context/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ch05_context;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ch05_Context";
+  }
+}
