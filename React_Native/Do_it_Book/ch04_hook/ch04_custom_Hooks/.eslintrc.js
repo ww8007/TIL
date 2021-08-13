2eb57a1c69fde944c6a3779ef6da0944e9ed39a6
ww8007 <wshmin@naver.com>
2021-08-13 15:21:04 +0900

    Feat : add eslint rules

--- React_Native/Do_it_Book/ch04_hook/ch04_custom_Hooks/.eslintrc.js
+++ React_Native/Do_it_Book/ch04_hook/ch04_custom_Hooks/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    curly: ['error', 'multi'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'none', // <--- THIS IS THE NEW RULE
+  },
 };
