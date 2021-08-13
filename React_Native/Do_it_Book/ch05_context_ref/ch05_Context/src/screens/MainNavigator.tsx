ac7a10945c55caba355e55aebc36d01dbe4171fb
ww8007 <wshmin@naver.com>
2021-08-14 02:33:17 +0900

    Study : add Navigator with home

---
+++ React_Native/Do_it_Book/ch05_context_ref/ch05_Context/src/screens/MainNavigator.tsx
@@ -0,0 +1,22 @@
+import React, {useState, useMemo} from 'react';
+import {BottomNavigation} from 'react-native-paper';
+import Home from './Home';
+
+export default function MainNavigator() {
+  const [index, setIndex] = useState<number>(0);
+  const routes = useMemo(
+    () => [{key: 'home', title: 'Home', icon: 'home'}],
+    [],
+  );
+
+  const renderScene = BottomNavigation.SceneMap({
+    home: Home,
+  });
+  return (
+    <BottomNavigation
+      navigationState={{index, routes}}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
+}
