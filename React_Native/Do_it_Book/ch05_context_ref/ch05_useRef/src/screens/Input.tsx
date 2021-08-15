9e066dfbb71566b4d3540d1be44bb84951ff80f6
ww8007 <wshmin@naver.com>
2021-08-15 22:10:57 +0900

    Study : useRef with keyboard

--- React_Native/Do_it_Book/ch05_context_ref/ch05_useRef/src/screens/Input.tsx
+++ React_Native/Do_it_Book/ch05_context_ref/ch05_useRef/src/screens/Input.tsx
@@ -1,17 +1,55 @@
-import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
-import {Colors} from 'react-native-paper'
-import * as D from '../data'
+import React, {useCallback, useRef, useState} from 'react';
+import {StyleSheet, View, Text, TextInput, Keyboard} from 'react-native';
+import {Switch} from 'react-native-paper';
+import {useTheme} from 'react-native-paper';
+import {useToggleTheme} from '../contexts';
+import * as D from '../data';
 
-const title = 'CopyMe'
 export default function CopyMe() {
+  const [person, setPerson] = useState<D.IPerson>(D.createRandomPerson());
+  const {dark, colors} = useTheme();
+  const toggleTheme = useToggleTheme();
+  const textInputRef = useRef<TextInput | null>(null);
+  const setFocus = useCallback(
+    () => textInputRef.current?.focus(),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [textInputRef.current],
+  );
   return (
-    <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
+    <View style={[styles.view, {backgroundColor: colors.surface}]}>
+      <View style={[styles.topBar, {backgroundColor: colors.accent}]}>
+        <Text style={[styles.textButton]} onPress={setFocus}>
+          focus
+        </Text>
+        <Text style={[styles.textButton]} onPress={Keyboard.dismiss}>
+          dismiss keyboard
+        </Text>
+        <View style={{flex: 1}} />
+        <Switch value={dark} onValueChange={toggleTheme} />
+      </View>
+      <View style={{flex: 1}} />
+      <View style={[styles.textView]}>
+        <Text style={[styles.textInput, {color: colors.text}]}>email</Text>
+        <TextInput
+          ref={textInputRef}
+          style={[
+            styles.textInput,
+            {color: colors.text, borderColor: colors.placeholder},
+          ]}
+          value={person.email}
+          placeholder="enter your email"
+          onChangeText={email => setPerson(person => ({...person, email}))}
+        />
+      </View>
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-  view: {flex: 1, padding: 5, backgroundColor: Colors.blue900},
+  view: {flex: 1},
+  topBar: {flexDirection: 'row', padding: 5},
+  textButton: {marginLeft: 10, fontSize: 20},
+  keyboardAvoidingView: {flex: 1, padding: 10},
+  textView: {padding: 5},
   text: {fontSize: 20, color: 'white'},
-})
+  textInput: {fontSize: 24, borderWidth: 1, borderRadius: 5},
+});
--- React_Native/Do_it_Book/ch05_context_ref/ch05_useRef/src/screens/KeyboardAware.tsx
+++ React_Native/Do_it_Book/ch05_context_ref/ch05_useRef/src/screens/KeyboardAware.tsx
@@ -1,17 +1,80 @@
-import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
-import {Colors} from 'react-native-paper'
-import * as D from '../data'
+import React, {useCallback, useMemo, useRef, useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import {Switch, useTheme} from 'react-native-paper';
+import {useToggleTheme} from '../contexts';
+import * as D from '../data';
 
-const title = 'CopyMe'
-export default function CopyMe() {
+export default function KeyboardAvoid() {
+  const [person, setPerson] = useState<D.IPerson>(D.createRandomPerson());
+  const {dark, colors} = useTheme();
+  const toggleTheme = useToggleTheme();
+  const textInputRef = useRef<TextInput | null>(null);
+  const setFocus = useCallback(
+    () => textInputRef.current?.focus(),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [textInputRef.current],
+  );
+  const textInputStyle = useMemo(
+    () => [
+      styles.textInput,
+      {color: colors.text, borderColor: colors.placeholder},
+    ],
+    [colors.text, colors.placeholder],
+  );
   return (
-    <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
+    <View style={[styles.view, {backgroundColor: colors.surface}]}>
+      <View style={[styles.topBar, {backgroundColor: colors.accent}]}>
+        <Text style={[styles.textButton]} onPress={setFocus}>
+          focus
+        </Text>
+        <Text style={[styles.textButton]} onPress={Keyboard.dismiss}>
+          dismiss keyboard
+        </Text>
+        <View style={{flex: 1}} />
+        <Switch value={dark} onValueChange={toggleTheme} />
+      </View>
+      <KeyboardAvoidingView
+        style={[styles.flex]}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <View style={[styles.flex]} />
+        <View style={[styles.textView]}>
+          <Text style={[styles.text, {color: colors.text}]}>email</Text>
+          <TextInput
+            style={textInputStyle}
+            value={person.email}
+            placeholder="enter your email"
+            onChangeText={email => setPerson(person => ({...person, email}))}
+          />
+        </View>
+        <View style={[styles.textView]}>
+          <Text style={[styles.text, {color: colors.text}]}>name</Text>
+          <TextInput
+            ref={textInputRef}
+            style={textInputStyle}
+            value={person.name}
+            placeholder="enter your name"
+            onChangeText={name => setPerson(person => ({...person, name}))}
+          />
+        </View>
+      </KeyboardAvoidingView>
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-  view: {flex: 1, padding: 5, backgroundColor: Colors.blue900},
-  text: {fontSize: 20, color: 'white'},
-})
+  view: {flex: 1},
+  topBar: {flexDirection: 'row', padding: 5},
+  textButton: {marginLeft: 10, fontSize: 20},
+  keyboardAvoidingView: {flex: 1, padding: 10},
+  textView: {padding: 5},
+  text: {fontSize: 24},
+  textInput: {fontSize: 20, borderWidth: 1, borderRadius: 5},
+  flex: {flex: 1},
+});
