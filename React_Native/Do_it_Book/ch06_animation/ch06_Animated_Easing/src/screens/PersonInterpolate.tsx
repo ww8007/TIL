ddcc886e27fead757d6d698785eebfe17632d522
ww8007 <wshmin@naver.com>
2021-08-19 11:35:33 +0900

    Study : learn interpolate in R/N

---
+++ React_Native/Do_it_Book/ch06_animation/ch06_Animated_Easing/src/screens/PersonInterpolate.tsx
@@ -0,0 +1,104 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useCallback} from 'react';
+import type {FC} from 'react';
+import {View, Text, Image, Animated, Easing} from 'react-native';
+import {Colors} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import moment from 'moment-with-locales-es6';
+// import {useToggle} from '../hooks'; // add
+import * as D from '../data';
+import {Avatar} from '../components';
+import {styles} from './Person.style';
+import {
+  useToggle,
+  useAnimatedValue,
+  useMonitorAnimatedValue,
+  useStyle,
+} from '../hooks';
+import {Text as ThemeText, View as ThemeView} from '../theme/paper';
+
+moment.locale('ko');
+
+export type PersonProps = {
+  person: D.IPerson;
+  deletePressed: () => void;
+};
+
+const PersonMonitor: FC<PersonProps> = ({person, deletePressed}) => {
+  const animValue = useAnimatedValue(0);
+  const realAnimValue = useMonitorAnimatedValue(animValue);
+  const [started, toggleStarted] = useToggle(false);
+  const avatarPressed = useCallback(() => {
+    Animated.timing(animValue, {
+      toValue: started ? 0 : 1,
+      useNativeDriver: false,
+      duration: 1000,
+      easing: Easing.bounce,
+    }).start(toggleStarted);
+  }, [started]);
+  const rightViewAnimStyle = useStyle({opacity: animValue});
+
+  const textAnimStyle = useStyle({
+    fontSize: animValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [10, 30],
+    }),
+    color: animValue.interpolate({
+      inputRange: [0, 0.7, 1],
+      outputRange: [Colors.lightBlue900, Colors.lime500, Colors.blue900],
+    }),
+  });
+  return (
+    <ThemeView>
+      <ThemeText style={[{fontSize: 20}]}>
+        realAnimValue: {realAnimValue}
+      </ThemeText>
+      <ThemeText style={[{fontSize: 20}]}>
+        started: {started ? 'true' : 'false'}
+      </ThemeText>
+      <View style={[styles.view]}>
+        <View style={[styles.leftView]}>
+          <Avatar
+            imageStyle={[styles.avatar]}
+            uri={person.avatar}
+            size={50}
+            onPress={avatarPressed}
+          />
+          <Text style={[styles.text]}>Press Me</Text>
+        </View>
+        <Animated.View style={[styles.rightView, rightViewAnimStyle]}>
+          <Animated.Text style={[styles.name, textAnimStyle]}>
+            {person.name}
+          </Animated.Text>
+          <Animated.Text style={[styles.email, textAnimStyle]}>
+            {person.email}
+          </Animated.Text>
+          <View style={[styles.dateView]}>
+            <Text style={[styles.text]}>
+              {moment(person.createdDate).startOf('day').fromNow()}
+            </Text>
+            <Icon
+              name="trash-can-outline"
+              size={26}
+              color={Colors.lightBlue500}
+              onPress={deletePressed}
+            />
+          </View>
+          <Text
+            numberOfLines={3}
+            ellipsizeMode="tail"
+            style={[styles.text, styles.comments]}>
+            {person.comments}
+          </Text>
+          <Image style={[styles.image]} source={{uri: person.image}} />
+          <View style={[styles.countsView]}>
+            <Icon name="comment" size={24} color={Colors.blue500} />
+            <Icon name="twitter-retweet" size={24} color={Colors.purple500} />
+            <Icon name="heart" size={24} color={Colors.red500} />
+          </View>
+        </Animated.View>
+      </View>
+    </ThemeView>
+  );
+};
+export default PersonMonitor;
