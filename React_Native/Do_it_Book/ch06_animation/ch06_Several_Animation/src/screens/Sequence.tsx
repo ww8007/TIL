bd6afe9536f3e8ee4c3506552a85f0e516f8e7ee
ww8007 <wshmin@naver.com>
2021-08-21 17:41:59 +0900

    Study : sequence animation 연속적으로 애니메이션 실행

--- React_Native/Do_it_Book/ch06_animation/ch06_Several_Animation/src/screens/Sequence.tsx
+++ React_Native/Do_it_Book/ch06_animation/ch06_Several_Animation/src/screens/Sequence.tsx
@@ -1,61 +1,55 @@
-import React, {useCallback, useState, useRef, useEffect, useMemo} from 'react';
-import type {FC} from 'react';
-import {View, Text, Image, Alert, Animated, Easing} from 'react-native';
-import {Colors} from 'react-native-paper';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import moment from 'moment-with-locales-es6';
-import {useToggle} from '../hooks'; // add
+import React, {useCallback, useState, useEffect} from 'react';
+import {View, Text, StyleSheet, Switch, FlatList} from 'react-native';
+import {useTheme} from 'react-native-paper';
+import {useToggleTheme} from '../contexts';
 import * as D from '../data';
-import {Avatar, IconText} from '../components';
-import {styles} from './Person.style';
+import Person from './PersonSequence';
 
-moment.locale('ko');
+export default function People() {
+  const [people, setPeople] = useState<D.IPerson[]>([]);
+  const theme = useTheme();
+  const toggleTheme = useToggleTheme();
+  const addPerson = useCallback(() => {
+    setPeople(people => [D.createRandomPerson(), ...people]);
+  }, []);
+  const removeAll = useCallback(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setPeople(notUsed => []);
+  }, []);
 
-export type PersonProps = {
-  person: D.IPerson;
-  deletePressed: () => void;
-};
-
-const Person: FC<PersonProps> = ({person, deletePressed}) => {
-  const avatarPressed = useCallback(() => Alert.alert('avatar pressed.'), []);
+  const deletePerson = useCallback(
+    (id: string) => () => {
+      setPeople(people => people.filter(person => person.id !== id));
+    },
+    [],
+  );
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(addPerson, []);
   return (
-    <View style={[styles.view]}>
-      <View style={[styles.leftView]}>
-        <Avatar
-          imageStyle={[styles.avatar]}
-          uri={person.avatar}
-          size={50}
-          onPress={avatarPressed}
-        />
-      </View>
-      <View style={[styles.rightView]}>
-        <Text style={[styles.name]}>{person.name}</Text>
-        <Text style={[styles.email]}>{person.email}</Text>
-        <View style={[styles.dateView]}>
-          <Text style={[styles.text]}>
-            {moment(person.createdDate).startOf('day').fromNow()}
-          </Text>
-          <Icon
-            name="trash-can-outline"
-            size={26}
-            color={Colors.lightBlue500}
-            onPress={deletePressed}
-          />
-        </View>
-        <Text
-          numberOfLines={3}
-          ellipsizeMode="tail"
-          style={[styles.text, styles.comments]}>
-          {person.comments}
+    <View style={[styles.view, {backgroundColor: theme.colors.surface}]}>
+      <View style={[styles.topBar, {backgroundColor: theme.colors.accent}]}>
+        <Text onPress={addPerson} style={styles.text}>
+          add
         </Text>
-        <Image style={[styles.image]} source={{uri: person.image}} />
-        <View style={[styles.countsView]}>
-          <Icon name="comment" size={24} color={Colors.blue500} />
-          <Icon name="twitter-retweet" size={24} color={Colors.purple500} />
-          <Icon name="heart" size={24} color={Colors.red500} />
-        </View>
+        <Text onPress={removeAll} style={styles.text}>
+          remove all
+        </Text>
+        <View style={{flex: 1}} />
+        <Switch value={theme.dark} onValueChange={toggleTheme} />
       </View>
+      <FlatList
+        data={people}
+        renderItem={({item}) => (
+          <Person person={item} deletePressed={deletePerson(item.id)} />
+        )}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
-};
-export default Person;
+}
+
+const styles = StyleSheet.create({
+  view: {flex: 1},
+  topBar: {flexDirection: 'row', padding: 5},
+  text: {marginRight: 10, fontSize: 20},
+});
