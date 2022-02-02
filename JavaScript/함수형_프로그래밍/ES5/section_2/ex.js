1aa34d8fc965be62ae5c72ea355f16b9609ff3a5
ww8007 <wshmin@naver.com>
2022-02-02 23:44:37 +0900

    Study: learn curry, curryr, get function

--- JavaScript/함수형_프로그래밍/ES5/section_2/ex.js
+++ JavaScript/함수형_프로그래밍/ES5/section_2/ex.js
@@ -1,3 +1,11 @@
+//////////////////////////////////////////
+// _filter, _map, _each
+// 기존에 존재하는 메서드가 있지만
+// 다음과 같이 클래스에 의존하지 않고
+// 함수형으로 설정하여
+// 모든 값에 대해서 유연성을 가지도록
+// 설정이 가능함
+
 function _filter(list, predi) {
 	const new_list = [];
 	for (let i = 0; i < list.length; i++) {
@@ -60,5 +68,57 @@ function _filter(list, predi) {
 		if (predi(val)) new_list.push(val);
 	});
 	return new_list;
-	ㅉㅉㅈ;
 }
+
+//////////////////////////////////////
+// _curry
+// 3항 연산자를 통한 리펙터링 된 함수
+// 인자가 두개 들어오면 즉시 실행 하도록
+// 함수를 다음과 같이 설정할 수 있음
+function _curry(fn) {
+	return function (a, b) {
+		return arguments.length === 2
+			? fn(a, b)
+			: function (b) {
+					return fn(a, b);
+			  };
+	};
+}
+
+// sub와 같은 함수에서
+// 실행 순서를 변경 시키고 싶을 때
+// curryr과 같은 함수를 설정하여서
+// 다음과 같이 사용이 가능하다
+function _curryr(fn) {
+	return function (a, b) {
+		return arguments.length === 2
+			? fn(a, b)
+			: function (b) {
+					return fn(b, a);
+			  };
+	};
+}
+
+const _sub = _curryr(function (a, b) {
+	return a - b;
+});
+
+const sub10 = _sub(10);
+console.log(sub10(5));
+
+//////////////////////////////////////
+// _get 함수
+function _get(obj, key) {
+	return obj === null ? undefined : obj[key];
+}
+
+console.log(users[10].name); // error
+console.log(_get(users, 'name')); //undefined
+
+// _curryr을 통한 리펙터링
+const _get = _curryr(function (obj, key) {
+	return obj === null ? undefined : obj[key];
+});
+
+const get_name = _get('name'); // 다음과 같이 name을 꺼낼 수 있는
+// 함수가 만들어짐
