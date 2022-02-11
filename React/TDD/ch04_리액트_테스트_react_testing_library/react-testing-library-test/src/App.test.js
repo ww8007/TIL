62154d6a2543a4b1aa0084da289647736335d097
ww8007 <wshmin@naver.com>
2022-02-12 02:51:14 +0900

    Study: learn react-testing-library

---
+++ React/TDD/ch04_리액트_테스트_react_testing_library/react-testing-library-test/src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('<App />', () => {
+	it('renders component correctly', () => {
+		const { container } = render(<App />);
+
+		const linkElement = screen.getByText(/learn react/i);
+		expect(linkElement).toBeInTheDocument();
+		expect(container.getElementsByTagName('p')).toHaveLength(1);
+		expect(container.getElementsByTagName('p')[0]).toHaveTextContent('test');
+		expect(container).toMatchSnapshot();
+	});
+});
