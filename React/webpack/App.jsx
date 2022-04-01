5ff0fcbedcb01db972e2c749687570620a529ca9
ww8007 <wshmin@naver.com>
2022-04-01 14:37:08 +0900

    Feat: add render example h1

---
+++ React/webpack/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div>
+			<h1>hi</h1>
+			<h1>hi</h1>
+			<h1>hi</h1>
+			<h1>hi</h1>
+			<h1>hi</h1>
+		</div>
+	);
+}
+
+export default App;
