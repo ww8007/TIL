a7e80e158ae5bc57755d8a3da5160da2492be62e
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-04 20:59:26 +0900

    solve: Using DFS with check arr

---
+++ Algorithm/Inflearn/자바스크립트_알고리즘_문제풀이/BFS_재귀함수/10_순열구하기.js
@@ -0,0 +1,26 @@
+function solution(m, arr) {
+	let answer = [];
+	n = arr.length;
+
+	let check = Array.from({ length: m }, () => 0);
+	let tmp = Array.from({ length: m }, () => 0);
+
+	function DFS(L) {
+		if (L === m) {
+			answer.push(tmp.slice());
+		} else {
+			for (let i = 0; i < n; i++) {
+				if (!check[i]) {
+					check[i] = 1; // 체크를 해서
+					tmp[L] = arr[i]; // 사용할 tmp에 넣고
+					DFS(L + 1); // 탐색을 돌리고
+					check[i] = 0; // 체크 해제
+				}
+			}
+		}
+	}
+	DFS(0);
+	return answer;
+}
+
+let arr = [3, 6, 9];
