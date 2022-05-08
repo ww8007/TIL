6455ce15edfdddc29f125b7dadf71cba72f7c7cc
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-08 21:19:36 +0900

    solve: Using DFS search

---
+++ Algorithm/Inflearn/자바스크립트_알고리즘_문제풀이/그래프와_탐색(DFS, BFS)/7_섬나라아일랜드_DFS.js
@@ -0,0 +1,38 @@
+function solution(board) {
+	let answer = 0;
+	let n = board.length;
+	let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+	let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+	function DFS(x, y) {
+		board[x][y] = 0;
+		for (let k = 0; k < 8; k++) {
+			let nx = x + dx[k];
+			let ny = y + dy[k];
+			if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+				DFS(nx, ny);
+			}
+		}
+	}
+	for (let i = 0; i < n; i++) {
+		for (let j = 0; j < n; j++) {
+			if (board[i][j] === 1) {
+				board[i][j] = 0;
+				answer++;
+				DFS(i, j);
+			}
+		}
+	}
+	return answer;
+}
+
+let arr = [
+	[1, 1, 0, 0, 0, 1, 0],
+	[0, 1, 1, 0, 1, 1, 0],
+	[0, 1, 0, 0, 0, 0, 0],
+	[0, 0, 0, 1, 0, 1, 1],
+	[1, 1, 0, 1, 1, 0, 0],
+	[1, 0, 0, 0, 1, 0, 0],
+	[1, 0, 1, 0, 1, 0, 0]
+];
+
+console.log(solution(arr));
