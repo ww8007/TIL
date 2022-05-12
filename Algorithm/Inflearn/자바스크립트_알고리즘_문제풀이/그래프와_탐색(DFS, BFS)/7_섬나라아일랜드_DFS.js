9739d01d6e1a6c9d2a4e29a0b5bf430b389435e1
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-13 07:59:46 +0900

    solve: Using DFS

--- Algorithm/Inflearn/자바스크립트_알고리즘_문제풀이/그래프와_탐색(DFS, BFS)/7_섬나라아일랜드_DFS.js
+++ Algorithm/Inflearn/자바스크립트_알고리즘_문제풀이/그래프와_탐색(DFS, BFS)/7_섬나라아일랜드_DFS.js
@@ -3,25 +3,30 @@ function solution(board) {
 	let n = board.length;
 	let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
 	let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
 	function DFS(x, y) {
 		board[x][y] = 0;
-		for (let k = 0; k < 8; k++) {
-			let nx = x + dx[k];
-			let ny = y + dy[k];
+		for (let i = 0; i < dx.length; i++) {
+			let nx = x + dx[i];
+			let ny = y + dy[i];
 			if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+				console.log(nx, ny);
 				DFS(nx, ny);
 			}
 		}
 	}
+
 	for (let i = 0; i < n; i++) {
 		for (let j = 0; j < n; j++) {
 			if (board[i][j] === 1) {
-				board[i][j] = 0;
+				// board[i][j] = 0;
 				answer++;
+
 				DFS(i, j);
 			}
 		}
 	}
+
 	return answer;
 }
 
