3fa0d4ffba619b7ff4e72073d45781034ee849a0
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-13 06:43:27 +0900

    solve: Using BFS level 탐색

--- Algorithm/Inflearn/자바스크립트_알고리즘_문제풀이/그래프와_탐색(DFS, BFS)/6_송아지찾기.js
+++ Algorithm/Inflearn/자바스크립트_알고리즘_문제풀이/그래프와_탐색(DFS, BFS)/6_송아지찾기.js
@@ -1,22 +1,52 @@
+// dist 배열을 통한 탐색
 function solution(s, e) {
-	let check = Array.from({ length: 10001 }, () => 0);
+	let ch = Array.from({ length: 10001 }, () => 0);
 	let dist = Array.from({ length: 10001 }, () => 0);
 
-	let queue = [];
-	queue.push(s);
-	dist[s] = 0;
+	let queue = [s];
 	while (queue.length) {
 		let x = queue.shift();
-		for (let nx of [x - 1, x + 1, x + 5]) {
+		for (const nx of [x - 1, x + 1, x + 5]) {
 			if (nx === e) return dist[x] + 1;
-			if (nx > 0 && nx < 10001 && check[nx] === 0) {
-				check[nx] = 1; // 방문
-				queue.push(nx); // 탐색에 집어 넣기
+			if (nx > 0 && nx < 10000 && ch[nx] === 0) {
+				ch[nx] = 1;
+				queue.push(nx);
 				dist[nx] = dist[x] + 1;
 			}
 		}
 	}
-	return dist[e];
+
+	return answer;
+}
+
+// 레벨을 통한 탐색
+// 현재 레벨을 모두 탐색 한다는 것이 특징
+function solution2(s, e) {
+	let answer = 0;
+
+	let ch = Array.from({ length: 10001 }, () => 0);
+
+	let queue = [s];
+	let L = 0;
+	while (queue.length) {
+		let len = queue.length;
+		for (let i = 0; i < len; i++) {
+			let x = queue.shift();
+			for (const nx of [x - 1, x + 1, x + 5]) {
+				if (nx === e) return L + 1;
+				if (nx > 0 && nx < 10000 && ch[nx] === 0) {
+					ch[nx] = 1;
+					queue.push(nx);
+				}
+			}
+		}
+		L += 1;
+	}
+
+	return answer;
 }
 
 console.log(solution(5, 14));
+console.log(solution2(5, 14));
+console.log(solution(8, 3));
+console.log(solution2(8, 3));
