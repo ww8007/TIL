c41bf667ebb1203f75884cbf7facdf44f3dce571
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-15 15:50:16 +0900

    solve: Using memo fibo

--- Algorithm/Inflearn/자바스크립트_알고리즘_문제풀이/BFS_재귀함수/11_팩토리얼.js
+++ Algorithm/Inflearn/자바스크립트_알고리즘_문제풀이/BFS_재귀함수/11_팩토리얼.js
@@ -10,4 +10,16 @@ function solution(n) {
 	return answer;
 }
 
+let fibArr = [1, 2];
+function memoFibo(n) {
+	function fibo(n) {
+		if (fibArr[n] !== undefined) return fibArr[n];
+		fibArr[n] = fibo(n) * fibo(n - 1);
+		return fibArr[n];
+	}
+	console.log(fibArr);
+	return fibo(n);
+}
+
 console.log(solution(5));
+console.log(memoFibo(5));
