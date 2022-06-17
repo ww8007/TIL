c76d8804b74d342ca731217c6b96dfd3da2f2837
장동현 <54137044+ww8007@users.noreply.github.com>
2022-06-17 23:05:51 +0900

    study: Learn using graphql

--- GraphQL/Example/index.js
+++ GraphQL/Example/index.js
@@ -20,34 +20,34 @@ const typeDefs = `
 `;
 
 const langs = [
-  {
-    id: 0,
-    name: 'Node',
-  },
-  {
-    id: 1,
-    name: 'Java',
-  },
+	{
+		id: 0,
+		name: 'Node'
+	},
+	{
+		id: 1,
+		name: 'Java'
+	}
 ];
 
 const resolvers = {
-  Query: {
-    getLangs: () => langs, //getLangs요청을 받으면 langs배열을 응답해줄 것입니다.
-  },
+	Query: {
+		getLangs: () => langs //getLangs요청을 받으면 langs배열을 응답해줄 것입니다.
+	}
 };
 
 const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
+	typeDefs,
+	resolvers
 });
 
 app.use('/graphql', bodyParser.json(), graphExpress({ schema }));
 
 app.use(
-  '/graphiql',
-  graphiqlExpress({
-    endpoint: '/graphiql',
-  })
+	'/graphiql',
+	graphiqlExpress({
+		endpoint: '/graphiql'
+	})
 );
 
 app.listen(PORT, () => console.log(`SERVER RUNNING AT ${PORT}`));
