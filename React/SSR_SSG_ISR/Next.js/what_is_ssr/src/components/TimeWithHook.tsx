2afe8c2802992b2aa34d24bcc9226977f75678d8
ww8007 <wshmin1234@gmail.com>
2022-10-04 00:43:03 +0900

    feat: Add 시간 렌더링 컴포넌트 1. 서버에서 불러온 시간 2. 실제 시간

--- React/SSR_SSG_ISR/Next.js/what_is_ssr/src/components/TimeWithHook.tsx
+++ React/SSR_SSG_ISR/Next.js/what_is_ssr/src/components/TimeWithHook.tsx
@@ -1,18 +1,30 @@
 import { css } from '@emotion/react';
-import React from 'react';
-import colors from '../constants/colors';
+
+import { useEffect, useState } from 'react';
 import useGetTime from '../hooks/useGetTime';
 
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+dayjs.locale('ko');
+
 interface Props {
-	dateTime: string;
+	dateTime: any;
 	title: string;
 	description: string;
 }
 
 function TimeWithHook({ dateTime, title, description }: Props) {
-	const cleanDate = dateTime;
+	const [formedDate, setFormedDate] = useState('');
+	const [realTime, setRealTime] = useState('');
 
-	const realTime = useGetTime();
+	const realTimeHook = useGetTime();
+
+	useEffect(() => {
+		setFormedDate(dayjs(dateTime).format('YYYY-MM-DD HH:mm:ss'));
+		setRealTime(realTimeHook);
+	}, [dateTime, realTimeHook]);
+
+	// const cleanDate = dateTime;
 
 	const flexStyle = css`
 		flex: 1;
@@ -28,7 +40,9 @@ function TimeWithHook({ dateTime, title, description }: Props) {
 			>
 				<li css={flexStyle}></li>
 				<li css={flexStyle}>
-					<h3>{cleanDate ? `${description} : ${cleanDate}` : 'LOADING...'}</h3>
+					<h3>
+						{formedDate ? `${description} : ${formedDate}` : 'LOADING...'}
+					</h3>
 				</li>
 				<li css={flexStyle}></li>
 			</ul>
@@ -39,7 +53,7 @@ function TimeWithHook({ dateTime, title, description }: Props) {
 			>
 				<li css={flexStyle}></li>
 				<li css={flexStyle}>
-					<h3>Real Time: {realTime}</h3>
+					<h3>Real Time: {realTime ? realTime : 'Loading'}</h3>
 				</li>
 				<li css={flexStyle}></li>
 			</ul>
