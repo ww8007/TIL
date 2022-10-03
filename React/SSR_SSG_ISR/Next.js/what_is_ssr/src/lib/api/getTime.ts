d989e186b81219af7ee71f9165ce0a90f5893b02
ww8007 <wshmin1234@gmail.com>
2022-10-04 00:45:49 +0900

    fix: Fix api endpoint

--- React/SSR_SSG_ISR/Next.js/what_is_ssr/src/lib/api/getTime.ts
+++ React/SSR_SSG_ISR/Next.js/what_is_ssr/src/lib/api/getTime.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 function getTimeAPI() {
-	return axios.get('https://worldtimeapi.org/api/ip');
+	return axios.get('https://worldtimeapi.org/api/timezone/Asia/Seoul');
 }
 export default getTimeAPI;
