6307a68a6ff0534775b3a5375cb3f5bd4fc9fff9
ww8007 <wshmin1234@gmail.com>
2022-10-04 00:39:02 +0900

    remove: 사용하지 않는 Head 제거

--- React/SSR_SSG_ISR/Next.js/what_is_ssr/pages/index.tsx
+++ React/SSR_SSG_ISR/Next.js/what_is_ssr/pages/index.tsx
@@ -1,6 +1,5 @@
 import { css } from '@emotion/react';
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Seo from '../src/components/Seo';
 import colors from '../src/constants/colors';
