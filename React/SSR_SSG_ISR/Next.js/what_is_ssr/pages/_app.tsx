0082f523d2f91e60873cd64a6523cd05d111c931
Classu-Ruben <ruben@classu.co.kr>
2022-10-31 23:30:22 +0900

    build: amplify 설정 추가

--- React/SSR_SSG_ISR/Next.js/what_is_ssr/pages/_app.tsx
+++ React/SSR_SSG_ISR/Next.js/what_is_ssr/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../GlobalStyle';
-
+import Amplify from 'aws-amplify';
+import awsmobile from '../src/aws-exports';
+Amplify.configure({ ...awsmobile, ssr: true });
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
