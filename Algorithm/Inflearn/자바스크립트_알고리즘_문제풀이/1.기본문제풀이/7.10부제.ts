864a591348e3f9a160c7106fe93862dd4d9b98eb
ww8007 <wshmin1234@gmail.com>
2022-12-20 02:56:18 +0900

    solve: with reduce

---
+++ Algorithm/Inflearn/자바스크립트_알고리즘_문제풀이/1.기본문제풀이/7.10부제.ts
@@ -0,0 +1,6 @@
+function solution(day: number, arr: number[]) {
+	return arr.reduce((acc, cur) => (cur % 10 === day ? ++acc : acc), 0)
+}
+
+const arr = [25, 23, 11, 47, 53, 17, 33]
+console.log(solution(3, arr))
