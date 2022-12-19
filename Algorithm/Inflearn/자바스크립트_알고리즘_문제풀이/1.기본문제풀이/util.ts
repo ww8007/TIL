0af41908750bbeecd094f06f209e30b90734cdaa
ww8007 <wshmin1234@gmail.com>
2022-12-20 02:57:31 +0900

    feat: get[Min, Max], getTotal, sum, range 관련 유틸 함수 추가

---
+++ Algorithm/Inflearn/자바스크립트_알고리즘_문제풀이/1.기본문제풀이/util.ts
@@ -0,0 +1,31 @@
+const getMinNumber = (arr: number[]) =>
+	arr.reduce((acc, cur) => (acc > cur ? cur : acc), Number.MAX_SAFE_INTEGER)
+
+const getMaxNumber = (arr: number[]) =>
+	arr.reduce((acc, cur) => (acc < cur ? cur : acc), Number.MIN_SAFE_INTEGER)
+
+const getTotal = (arr: number[]) => arr.reduce(sum, 0)
+
+const sum = (a: number, b: number) => a + b
+
+const range = (l: number) => {
+	let i = -1
+	const arr = []
+	while (++i < l) arr.push(i)
+	return arr
+}
+
+const getOddNumbers = (arr: number[]) => arr.filter((number) => number % 2)
+
+const filterMaxNumber = (arr: number[]) =>
+	[...arr].filter((num) => num !== getMaxNumber(arr))
+
+export {
+	getMinNumber,
+	getMaxNumber,
+	getTotal,
+	sum,
+	range,
+	getOddNumbers,
+	filterMaxNumber
+}
