abe23925d3cdd87fc651a2efcfb6307df485279f
ww8007 <wshmin1234@gmail.com>
2023-01-18 23:10:05 +0900

    build: 초기 setting

---
+++ Java/React_SpringBoot_101/ch02/src/main/java/com/example/ch02/Ch02Application.java
@@ -0,0 +1,13 @@
+package com.example.ch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch02Application.class, args);
+	}
+
+}
---
+++ Java/React_SpringBoot_101/ch02/src/test/java/com/example/ch02/Ch02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
