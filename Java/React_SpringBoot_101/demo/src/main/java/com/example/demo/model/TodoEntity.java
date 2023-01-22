aaf8c01649961fb7065b8421da5618f2a552c4df
ww8007 <wshmin1234@gmail.com>
2023-01-23 04:44:16 +0900

    feat: TodoEntity 생성 uuid 기반 id

---
+++ Java/React_SpringBoot_101/demo/src/main/java/com/example/demo/model/TodoEntity.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "Todo")
+
+public class TodoEntity {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+    private String userId;
+    private String title;
+    private boolean done;
+}
