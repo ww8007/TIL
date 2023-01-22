1e8f950b3d8368359a1e3a8b0f43a73e6684edbc
ww8007 <wshmin1234@gmail.com>
2023-01-23 04:44:55 +0900

    feat: TodoRepository 생성 findByUserId 추가

---
+++ Java/React_SpringBoot_101/demo/src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.persistence;
+
+import com.example.demo.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    List<TodoEntity> findByUserId(String userId);
+}
