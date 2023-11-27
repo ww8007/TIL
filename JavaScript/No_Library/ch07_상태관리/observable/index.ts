fce89f40d90b8c899c1dd775cfcf4c3b172d9b23
ww8007 <wshmin1234@gmail.com>
2023-11-27 10:04:17 +0900

    feat: add logger, localStorage listeners

--- JavaScript/No_Library/ch07_상태관리/observable/index.ts
+++ JavaScript/No_Library/ch07_상태관리/observable/index.ts
@@ -1,21 +1,30 @@
 import registry from "./core/registry";
-import { CustomEvents, State } from "./core/types";
+import { State } from "./core/types";
 import appView from "./view/app";
 import todosView from "./view/todos";
 import counterView from "./view/counter";
 import filtersView from "./view/filters";
 import applyDiff from "./core/applyDiff";
-
-import modelFactory from "./model/model";
+import stateFactory from "./model/state";
 
 registry.add("app", appView);
 registry.add("todos", todosView);
 registry.add("counter", counterView);
 registry.add("filters", filtersView);
 
-const model = modelFactory();
+const loadState = () => {
+	const serializedState = window.localStorage.getItem("state");
 
-const { addChangeListener, ...events } = model;
+	if (!serializedState) {
+		return;
+	}
+
+	return JSON.parse(serializedState);
+};
+
+const state = stateFactory(loadState());
+
+const { addChangeListener, ...events } = state;
 
 const render = (state: State) => {
 	window.requestAnimationFrame(() => {
@@ -28,3 +37,13 @@ const render = (state: State) => {
 };
 
 addChangeListener(render);
+
+addChangeListener((state: State) => {
+	Promise.resolve().then(() => {
+		window.localStorage.setItem("state", JSON.stringify(state));
+	});
+});
+
+addChangeListener((state: State) => {
+	console.log(`Current State (${new Date().getTime()})`, state);
+});
